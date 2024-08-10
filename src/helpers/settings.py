c17f8aba3f5aa41633082664f8acd69797d5c8e8
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2024-08-10 16:27:40 -0300

    feat(settings): create helper to load settings

---
+++ src/helpers/settings.py
@@ -0,0 +1,26 @@
+from json import loads
+
+
+def init_setup(file_path: str) -> dict[str, str]:
+    with open(file_path) as file_ref:
+        SETTINGS: dict = loads(file_ref.read())
+        file_ref.close()
+
+    return {
+        'SSID': SETTINGS.get('wifi').get('ssid'),
+        'PSWD': SETTINGS.get('wifi').get('password'),
+
+        'THINGSPEAK_CHANNEL_CODE': SETTINGS\
+            .get('thingspeak')\
+            .get('channel'),
+
+        'THINGSPEAK_API_KEY_READ': SETTINGS\
+            .get('thingspeak')\
+            .get('apikey')\
+            .get('read'),
+
+        'THINGSPEAK_API_KEY_WRITE': SETTINGS\
+            .get('thingspeak')\
+            .get('apikey')\
+            .get('read')
+    }
