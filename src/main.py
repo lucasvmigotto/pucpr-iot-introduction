6ee5d7428ebe9bff8ab7310633cec99fa9555193
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2024-08-10 17:00:27 -0300

    feat(main): test dht11 sensor

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import sys
-from handlers.wifi import WiFiHandler
+from time import sleep
+from machine import Pin
+from dht import DHT11
 from helpers.settings import init_setup
+
 
 SETTINGS_FILE_PATH: str = './settings.json'
 
@@ -9,15 +12,26 @@ def main() -> None:
 
         SETTINGS: dict = init_setup(SETTINGS_FILE_PATH)
 
-        with WiFiHandler(SETTINGS.get('SSID'), SETTINGS.get('PSWD')) as wifi:
+        sensor: DHT11 = DHT11(Pin(SETTINGS.get('SENSORS').get('dht11')))
 
-            print(wifi.wlan.status())
+        PRINT_TEMPLATE: str = '🌡️: {temperature:.2f}\n💧: {humidity:.2f}'
+
+        while True:
+            sensor.measure()
+
+            print(PRINT_TEMPLATE.format(
+                temperature=sensor.temperature(),
+                humidity=sensor.humidity()
+            ), end=f"\n{'=' * 10}\n")
+
+            sleep(1 * .5)
 
     except KeyboardInterrupt:
         sys.exit(0)
 
     except Exception as err:
         print(err)
+        sys.exit(1)
 
 
 if __name__ == '__main__':
