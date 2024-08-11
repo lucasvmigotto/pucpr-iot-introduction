e18d30061ab0f22c07be7127657e0df98ce7b19c
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2024-08-11 17:34:44 -0300

    feat(main): update main file
    Connect to wifi network, read dht11 sensor and push data to thingspeak

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import sys
+from logging import Logger, getLogger
 from time import sleep
 from machine import Pin
 from components.dht11 import DHT11
 from helpers.settings import Settings
-
-
-SETTINGS_FILE_PATH: str = './settings.json'
+from defaults import SETTINGS_FILE_PATH
+from handlers.wifi import WiFiHandler
+from services.thingspeak import ThingSpeakService
+from utils.main import init_logging
 
 
 def main() -> None:
@@ -13,23 +15,29 @@ def main() -> None:
 
         SETTINGS: Settings = Settings(SETTINGS_FILE_PATH)
 
+        init_logging(SETTINGS)
+        logger: Logger = getLogger(__name__)
+
         DHT11_PIN: int = SETTINGS.get_config('sensors', 'dht11')
         SLEEP_INTERVAL: int = SETTINGS.get_config('definitions', 'sleep_interval')
 
         sensor: DHT11 = DHT11(Pin(DHT11_PIN))
+        service: ThingSpeakService = ThingSpeakService(SETTINGS)
 
-        while True:
-            sensor.refresh()
+        with WiFiHandler(SETTINGS) as wifi:
+            while True:
+                sensor.refresh()
 
-            print(sensor)
+                service.push_to_channel(sensor)
 
-            sleep(SLEEP_INTERVAL)
+                sleep(SLEEP_INTERVAL)
 
     except KeyboardInterrupt:
+        logger.info('Exiting...')
         sys.exit(0)
 
     except Exception as err:
-        print(err)
+        logger.exception(err)
         sys.exit(1)
 
 
