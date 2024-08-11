24dac6ea43451123c3b0d21079bbbaaa40234fcd
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2024-08-11 17:31:50 -0300

    feat(utils): create init_logging config function

--- src/utils/main.py
+++ src/utils/main.py
@@ -1,14 +1,22 @@
+from logging import basicConfig, DEBUG, INFO
+from helpers.settings import Settings
 from time import localtime
-
+from defaults import UTILS_DATETIME_FORMAT
 
 def datetime_now() -> str:
     year, month, day, hour, minute, second, *_ = localtime()
 
-    return '{Y}-{M:02}-{d:02} {h:02}:{m:02}:{s:02}'.format(
+    return UTILS_DATETIME_FORMAT.format(
         Y=str(year),
         M=month,
         d=day,
         h=hour,
         m=minute,
         s=second
+    )
+
+def init_logging(settings: Settings) -> None:
+    basicConfig(
+        level=DEBUG if settings.get_config('logging', 'level') == 'DEBUG' else INFO,
+        format=settings.get_config('logging', 'format')
     )
