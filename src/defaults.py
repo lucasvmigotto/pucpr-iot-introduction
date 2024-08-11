9f745fdcb67d0892aacbffb03ddf55303c9750fe
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2024-08-11 17:27:56 -0300

    feat(defaults): add logging format string and setting file path

--- src/defaults.py
+++ src/defaults.py
@@ -1,5 +1,6 @@
 CONNECT_RETRY_TIMEOUT: int = 1
 THINGSPEAK_BASE_URL: str = 'https://api.thingspeak.com'
-SETTINGS_FILEPATH = 'settings.json'
+SETTINGS_FILE_PATH: str = './settings.json'
 TEMPERATURE_ICON: str = '🌡️'
 HUMIDITY_ICON: str = '💧'
+UTILS_DATETIME_FORMAT: str = '{Y}-{M:02}-{d:02}T{h:02}:{m:02}:{s:02}'
