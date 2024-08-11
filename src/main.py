ec5c47825c487cca3f709166f004041ccd0456b5
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2024-08-11 18:34:33 -0300

    feat(main): add relay to main file
    Add switch control depending the dht11 data values

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import sys
 from logging import Logger, getLogger
 from time import sleep
-from machine import Pin
 from components.dht11 import DHT11
+from components.relay import Relay
 from helpers.settings import Settings
 from defaults import SETTINGS_FILE_PATH
 from handlers.wifi import WiFiHandler
@@ -19,16 +19,26 @@ def main() -> None:
         logger: Logger = getLogger(__name__)
 
         DHT11_PIN: int = SETTINGS.get_config('sensors', 'dht11')
+        RELAY_PIN: int = SETTINGS.get_config('sensors', 'relay')
         SLEEP_INTERVAL: int = SETTINGS.get_config('definitions', 'sleep_interval')
+        TEMPERATURE_THRESHOLD: int = SETTINGS.get_config('definitions', 'temperature_threshold')
+        HUMIDITY_THRESHOLD: int = SETTINGS.get_config('definitions', 'humidity_threshold')
 
-        sensor: DHT11 = DHT11(Pin(DHT11_PIN))
+        sensor: DHT11 = DHT11(DHT11_PIN)
+        relay: Relay = Relay(RELAY_PIN)
         service: ThingSpeakService = ThingSpeakService(SETTINGS)
 
-        with WiFiHandler(SETTINGS) as wifi:
+        with WiFiHandler(SETTINGS) as _wifi:
             while True:
                 sensor.refresh()
 
                 service.push_to_channel(sensor)
+
+                relay.switch_by_dht11_state(
+                    sensor,
+                    TEMPERATURE_THRESHOLD,
+                    HUMIDITY_THRESHOLD
+                )
 
                 sleep(SLEEP_INTERVAL)
 
